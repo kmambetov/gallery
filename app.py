5290d262ba6cf80fc89e2a76f4863c7e665427e0
kmambetov <%kmambetov%>
2020-04-23 17:36:41 +0600

    First commit

--- app.py
+++ app.py
@@ -4,15 +4,22 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    f = open("urls.txt", "r", encoding="utf-8")
+    urls = [row for row in f]
+    f.close()
+    return render_template("index.html", urls=urls)
 
 
 @app.route("/add")
 def add():
     return render_template("form.html")
 
+
 @app.route("/receiver", methods=["POST"])
 def receiver():
     url = request.form.get("url")
-    print(url)
-    return render_template("index.html")   
+    f = open("urls.txt", "a+", encoding="utf-8")
+    f.write("\n" + url )
+    sources = [row for row in f if row]
+    f.close()
+    return render_template("index.html", url=sources)      
